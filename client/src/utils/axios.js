6100aa377ad511c5e68832995ea2f6b5af99d5f1
amal_yuldashev <amalyuldashev@gmail.com>
2025-05-21 23:24:43 +0200

    changed base URL axios

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,13 +1,12 @@
 import axios from "axios";
-import { API_URL } from "../config";
 
 const instance = axios.create({
-  baseURL: `${API_URL}api`,
+  baseURL: "/api",
+  withCredentials: true,
 });
 
 instance.interceptors.request.use((config) => {
   config.headers.Authorization = window.localStorage.getItem("token");
-
   return config;
 });
 
