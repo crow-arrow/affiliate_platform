86a7c7ab1ace1dfecd0371d05a5dfb8a39b48363
amal_yuldashev <amalyuldashev@gmail.com>
2025-05-22 08:57:48 +0200

    base URL changed in config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = "http://localhost:3002/";
+export const API_URL = "https://partner.jinn-travel.com/";
--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { API_URL } from "../config";
 
 const instance = axios.create({
-  baseURL: "/api",
+  baseURL: `${API_URL}api`,
   withCredentials: true,
 });
 
