4da50942650aa849946c31f6887b74db36749960
amal_yuldashev <amalyuldashev@gmail.com>
2025-05-22 09:14:31 +0200

    404 img optimized

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import notFoundImage from "../assets/404.jpg"
+import notFoundImage from "../assets/404.avif"
 
 export const NotFound = () => (
     <div     
