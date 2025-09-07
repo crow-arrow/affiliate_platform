c6b43f99d126fcc5a46c76aa1f570860bd7a5f81
amal_yuldashev <amalyuldashev@gmail.com>
2025-09-07 14:42:32 +0200

    test env

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -6,11 +6,24 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    define: {
+      "import.meta.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
+      "import.meta.env.VITE_CLERK_PUBLISHABLE_KEY": JSON.stringify(
+        env.VITE_CLERK_PUBLISHABLE_KEY
+      ),
+      "import.meta.env.VITE_SHOW_DEV_BANNER": JSON.stringify(
+        env.VITE_SHOW_DEV_BANNER
+      ),
+    },
+  };
 });
