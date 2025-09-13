51527ac0a09dbd13700555f52cc6ad3b10246176
amal_yuldashev <amalyuldashev@gmail.com>
2025-09-13 14:05:44 +0200

    change clerk key to live

--- client/src/AppProvider.jsx
+++ client/src/AppProvider.jsx
@@ -4,6 +4,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { useMuiMode } from "./data_grid_theme/useMuiMode";
 import { themeSettings } from "./data_grid_theme/theme.js";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { useNavigate } from "react-router-dom";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -16,10 +17,11 @@ if (!PUBLISHABLE_KEY) {
 const AppProvider = () => {
   const mode = useMuiMode();
   const theme = createTheme(themeSettings(mode));
+  const navigate = useNavigate();
 
   return (
     <ThemeProvider theme={theme}>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} navigate={navigate}>
         <CssBaseline />
         <App />
       </ClerkProvider>
