0d6727c7340d2e46969084ba5464236db9d4abea
amal_yuldashev <amalyuldashev@gmail.com>
2025-09-14 14:07:01 +0200

    +

--- client/src/components/verification/OAuthCallback.jsx
+++ client/src/components/verification/OAuthCallback.jsx
@@ -1,17 +1,3 @@
-import {
-  AuthenticateWithRedirectCallback,
-  useUser,
-  useAuth,
-} from "@clerk/clerk-react";
-import { useEffect, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  loginWithOAuth,
-  checkIsAuth,
-} from "../../redux/features/auth/authSlice.js";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-
 export const OAuthCallback = () => {
   const { isSignedIn, user: clerkUser } = useUser();
   const { getToken } = useAuth();
@@ -19,7 +5,6 @@ export const OAuthCallback = () => {
   const navigate = useNavigate();
   const isAuth = useSelector(checkIsAuth);
 
-  // Чтобы не запускать useEffect несколько раз
   const calledRef = useRef(false);
 
   useEffect(() => {
@@ -29,29 +14,26 @@ export const OAuthCallback = () => {
     console.log("isAuth:", isAuth);
 
     const run = async () => {
-      if (calledRef.current) {
-        console.log("⏭️ Уже вызывали, пропускаем");
-        return;
-      }
+      if (calledRef.current) return;
       if (isSignedIn && clerkUser && !isAuth) {
-        console.log("✅ Условия выполнены, идем за токеном");
         calledRef.current = true;
         try {
           const token = await getToken();
           console.log("Token:", token?.substring(0, 20), "...");
           await dispatch(loginWithOAuth({ token })).unwrap();
           console.log("✅ loginWithOAuth вызван");
+          toast.success("Успешный вход через OAuth");
+          navigate("/my-account");
         } catch (e) {
           console.error("❌ Ошибка:", e);
+          toast.error("Ошибка при входе через OAuth");
+          navigate("/sign-in");
         }
-      } else {
-        console.log("⏳ Условия не выполнены");
       }
     };
 
     run();
-  }, [isSignedIn, clerkUser, isAuth, getToken, dispatch]);
+  }, [isSignedIn, clerkUser, isAuth, getToken, dispatch, navigate]);
 
-  // Эта страница всегда должна рендерить компонент коллбэка
   return <AuthenticateWithRedirectCallback />;
 };
