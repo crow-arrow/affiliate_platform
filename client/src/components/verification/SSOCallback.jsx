77939e2d2855789c80dd08afdbb7b80822ef5e06
amal_yuldashev <amalyuldashev@gmail.com>
2025-09-14 16:29:53 +0200

    +

--- client/src/components/verification/SSOCallback.jsx
+++ client/src/components/verification/SSOCallback.jsx
@@ -1,15 +1,7 @@
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
 
 export const SSOCallback = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    toast.info("Redirecting to complete sign-in...");
-    navigate("/sign-in/oauth-done");
-  }, [navigate]);
-
-  return <AuthenticateWithRedirectCallback />;
+  return (
+    <AuthenticateWithRedirectCallback signInForceRedirectUrl="/sign-in/oauth-done" />
+  );
 };
